55c5a691ae51647483b2bef63b34c1fa4e6e0c13
Ryan Bagan <darcknight1980@gmail.com>
2023-05-03 10:41:21 -0700

    100 percent done

---
+++ src/routes/v1.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const express = require('express');
+const dataModules = require('../auth/models/index');
+const router = express.Router();
+
+router.param('model', (req, res, next) => {
+  const modelName = req.params.model;
+  if (dataModules[modelName]) {
+    req.model = dataModules[modelName];
+    next();
+  } else {
+    next('Model Invalid');
+  }
+});
+
+router.get('/:model', handleGetAll);
+router.get('/:model/:id', handleGetOne);
+router.post('/:model', handleCreate);
+router.put('/:model/:id', handleUpdate);
+router.delete('/:model/:id', handleDelete);
+
+async function handleGetAll(req, res) {
+  let allRecords = await req.model.get();
+  res.status(200).json(allRecords);
+}
+
+async function handleGetOne(req, res) {
+  const id = req.params.id;
+  let oneRecord = await req.model.get(id);
+  res.status(200).json(oneRecord);
+}
+
+async function handleCreate(req, res) {
+  console.log(req);
+  let obj = req.body;
+  console.log('Should return an object: ',obj);
+  let newRecord = await req.model.create(obj);
+  console.log(newRecord);
+  res.status(201).json(newRecord);
+}
+
+async function handleUpdate(req, res) {
+  const id = req.params.id;
+  const obj = req.body;
+  let updateRecord = await req.model.update(id, obj);
+  res.status(200).json(updateRecord);
+}
+
+async function handleDelete(req, res) {
+  let id = req.params.id;
+  let deleteRecord = await req.model.delete(id);
+  res.status(200).json(deleteRecord);
+}
+
+module.exports = router;
