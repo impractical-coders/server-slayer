759f79e4a5d181f9553586b77d936c5e099c95ac
Thomas Lau <tomslau@outlook.com>
2023-05-03 23:34:09 -0700

    vote is done

--- clients/player/index.js
+++ clients/player/index.js
@@ -20,6 +20,30 @@ let playerData = {
   role : null,
   playersInCurrentRoom : null,
   choice : 0,
+  tasks: [
+    'Found a whiteboard.',
+    'Found the exit!',
+    'Found a computer, but there is no power.',
+    'The windows are barred.',
+    'Why is Mona Lisa here?',
+    'There is a bed here.',
+    'Radio says there is a zombie outbreak.',
+    'Cookie Monster!',
+    'This is a very nice fountain.',
+    'Found Cody\'s resume, must hire him when I get out...',
+    'I can see the Statue of Liberty from here.',
+    'The TV is playing SpongeBob.',
+    'Found the game StarCraft 3.',
+    'This house is very old.',
+    'Found Ryan\'s resume, must hire him when I get out...',
+    'Who is this Rapib guy?',
+    'A tiger just walked pass me, need to keep quiet.',
+    'It is 1am in the morning',
+    'Found Thomas\' resume, must hire him when I get out...',
+    'What am I even looking for?',
+    'Must take a nap, kind of tired walking around.',
+    'Found Kameron\'s resume, must hire him when I get out...',
+  ],
 };
 
 
@@ -38,9 +62,10 @@ gameSocket.on('globalEvent', (msg)=>{
 // vote result action
 gameSocket.on('voteResult', (maxVotePlayer)=>{
   if (maxVotePlayer[0] === playerData.name){
-    console.log('You have been kicked out! [Game Over]');
+    console.log('[Game Over] You have been kicked out!');
     gameSocket.disconnect(playerData.name);
   }
+
 });
 
 // getting lobby status
@@ -66,7 +91,7 @@ gameSocket.on('vote', (msg, aliveArr)=>{
     console.log(msg);
     vote(aliveArr, playerData,gameSocket);
   } else {
-    console.log('You have been killed! [Game Over]');
+    console.log('[Game Over] You have been killed!');
     //TODO: show result? update score? clear prompt
     gameSocket.disconnect(playerData.name);
   }
--- server/index.js
+++ server/index.js
@@ -120,20 +120,54 @@ game.on('connection', (socket) => {
     socket.emit('myRole', role);
   });
 
-  // slayer kill
-  socket.on('playerKill', (personToBeKilled) => {
+  socket.on('won', (name, currentRoom)=>{
+    game.emit('globalEvent', `${name} escaped! (And didn't tell anyone where the exit is... What a *#$%#.)`);
     for (let i = 0; i < currentPlayers.length; i++) {
-      if (personToBeKilled === currentPlayers[i].name) {
+      if (name === currentPlayers[i].name) {
+        currentPlayers[i].role = 'escaped';
+      }
+    }
+    let idx0 = insideRoom[currentRoom].indexOf(name);
+    let updatedInsideRoom = insideRoom[currentRoom].splice(idx0, 1);
+    game.to(currentRoom).emit('roomStatus', updatedInsideRoom, currentRoom);
+    let idx = aliveArr.indexOf(name);
+    aliveArr.splice(idx, 1);
+    if (aliveArr.length === 1 && aliveArr.includes(slayer)){
+      let msg = `[Game Over] Last survivor escaped, Slayer ${slayer} lost!`;
+      game.emit('globalEvent', msg);
+      game.disconnectSockets(true);
+    }
+    if (aliveArr.length === 2 && aliveArr.includes(slayer)){
+      let msg = 'Sudden Death: Last survivor must find the exit beofre the slayer finds you!';
+      game.emit('globalEvent', msg);
+    }
+  });
+
+  // slayer kill
+  socket.on('playerKill', (action) => {
+    for (let i = 0; i < currentPlayers.length; i++) {
+      if (action[0] === currentPlayers[i].name) {
         currentPlayers[i].role = 'dead';
       }
     }
-    console.log('139', currentPlayers);
-    deadArr.push(personToBeKilled);
-    let idx = aliveArr.indexOf(personToBeKilled);
+    // console.log(`130 rooms status: ${JSON.stringify(insideRoom, null, 2)}`);
+    let idx0 = insideRoom[`${action[1]}`].indexOf(action[0]);
+    let updatedInsideRoom = insideRoom[`${action[1]}`].splice(idx0, 1);
+    game.to(`${action[1]}`).emit('roomStatus', updatedInsideRoom, `${action[1]}`);
+    // console.log(`130 rooms status AFTER: ${JSON.stringify(insideRoom, null, 2)}`);
+    // console.log('139', currentPlayers);
+    deadArr.push(action[0]);
+    let idx = aliveArr.indexOf(action[0]);
     aliveArr.splice(idx, 1);
-    console.log('dead', deadArr);
-    console.log('alive140', aliveArr);
-    let msg = `[ALERT] ${personToBeKilled} is found DEAD!`;
+    // console.log('dead', deadArr);
+    // console.log('alive140', aliveArr);
+    if (aliveArr.length === 1){
+      socket.broadcast.emit('globalEvent', 'You have been killed!');
+      let msg = `[Game Over] No more survivor, Slayer ${slayer} won!`;
+      game.emit('globalEvent', msg);
+      return game.disconnectSockets(true);
+    }
+    let msg = `[ALERT] ${action[0]} is found DEAD!`;
     game.emit('vote', msg, aliveArr);
   });
 
@@ -163,11 +197,11 @@ game.on('connection', (socket) => {
   });
 
   socket.on('voteResult', (vote) => {
-    console.log('165res', resultArr.length);
+    // console.log('165res', resultArr.length);
     resultArr.push(vote);
-    console.log('vote', resultArr);
+    // console.log('vote', resultArr);
     // console.log(`vote results ${JSON.stringify(counts, null, 2)}`);
-    console.log(aliveArr);
+    // console.log(aliveArr);
 
     if (resultArr.length === aliveArr.length) {
       resultArr.forEach(element => {
@@ -190,30 +224,49 @@ game.on('connection', (socket) => {
           maxVotePlayer.push(player);
         }
       }
-      console.log('maxVotePlayer', maxVotePlayer);
+      // console.log('maxVotePlayer', maxVotePlayer);
       if (maxVotePlayer.length > 1) {
         let msg = `TIED: Nobody got kicked out. Game continues...`;
         game.emit('globalEvent', msg);
+        // game.to(roomChange).emit('roomStatus', currentRoomPlayers, roomChange);
+        socket.emit('playerAction', 'nth');
       } else {
         if (maxVotePlayer[0] === slayer) {
-          let msg = `Congratulations, SLAYER has been caught! [Game Over]`;
+          let msg = `[Game Over] Congratulations, SLAYER has been caught!`;
           game.emit('globalEvent', msg);
           game.disconnectSockets(true);
         } else if (maxVotePlayer[0] === '[SKIP]'){
           let msg = `Nobody got kicked out. Game continues...`;
           game.emit('globalEvent', msg);
         } else {
-          let msg = `${maxVotePlayer} has been kicked out, but SLAYER is still out there! Game continues...`;
+          let msg = `${maxVotePlayer[0]} has been kicked out, but SLAYER is still out there! Game continues...`;
+          for (let i = 0; i < currentPlayers.length; i++) {
+            if (maxVotePlayer[0] === currentPlayers[i].name) {
+              currentPlayers[i].role = 'kicked';
+            }
+          }
+          let room = null;
+          for (const key in insideRoom) {
+            if (Array.isArray(insideRoom[key]) && insideRoom[key].includes(maxVotePlayer[0])) {
+              room = key;
+            }
+          }
+          let idx0 = insideRoom[room].indexOf(maxVotePlayer[0]);
+          let updatedInsideRoom = insideRoom[room].splice(idx0, 1);
+          game.to(room).emit('roomStatus', updatedInsideRoom, room);
           kickArr.push(maxVotePlayer[0]);
+          console.log(aliveArr);
           let idx = aliveArr.indexOf(maxVotePlayer[0]);
           aliveArr.splice(idx, 1);
-          if (aliveArr.length === 2){
-            let msg = 'DRAW: 1[Game Over]';
-            game.emit('globalEvent', msg);
-            game.disconnectSockets(true);
-          }
+          console.log(aliveArr);
           game.emit('globalEvent', msg);
+          if (aliveArr.length === 2){
+            let msg = 'Sudden Death: Last survivor must find the exit beofre the slayer finds you!';
+            game.emit('globalEvent', msg);
+          }
         }
+        // game.to(roomChange).emit('roomStatus', currentRoomPlayers, roomChange);
+        game.emit('playerAction', 'nth');
       }
       game.emit('voteResult', maxVotePlayer);
       resultArr = [];
@@ -227,7 +280,7 @@ game.on('connection', (socket) => {
   // });
   socket.on('disconnect', () => {
     if (socket.username === slayer) {
-      game.emit('globalEvent', 'Slayer has left the game. The game will now end.');
+      game.emit('globalEvent', '[Game Over] Slayer has left the game.');
       game.disconnectSockets(true);
     }
     let idx = currentPlayers.findIndex(obj => obj.name === socket.username);
